b1c3fcc3c6fc1efe9055d0af30c0d22d22d52f3c
Litmanowski <marcin.litmanowski@nokia.com>
2020-10-03 08:42:05 +0200

    Add mumjolandia console on Home page
    Python's mumjolandia is now capable of running local server for handling
    all possible commands via socket communication. Server only accepts local
    connections so ip is hardcoded to localhost. It is now possible to execute
    every command existing in python's mumjolandia remotely.

---
+++ app/src/main/java/com/example/mumjolandiaandroid/utils/Helpers.java
@@ -0,0 +1,21 @@
+package com.example.mumjolandiaandroid.utils;
+
+import android.os.Build;
+
+public class Helpers {
+    public static Boolean isEmulator(){
+            return ((Build.FINGERPRINT.startsWith("google/sdk_gphone_") && Build.FINGERPRINT.endsWith(":user/release-keys")
+                    && Build.MANUFACTURER == "Google" && Build.PRODUCT.startsWith("sdk_gphone_") && Build.BRAND == "google"
+                    && Build.MODEL.startsWith("sdk_gphone_")) // Android SDK emulator
+                    || Build.FINGERPRINT.startsWith("generic")
+                    || Build.FINGERPRINT.startsWith("unknown")
+                    || Build.MODEL.contains("google_sdk")
+                    || Build.MODEL.contains("Emulator")
+                    || Build.MODEL.contains("Android SDK built for x86")
+                    || "QC_Reference_Phone" == Build.BOARD  //bluestacks
+                    || Build.MANUFACTURER.contains("Genymotion")
+                    || Build.HOST.startsWith("Build") //MSI App Player
+                    || Build.BRAND.startsWith("generic") && Build.DEVICE.startsWith("generic")
+                    || Build.PRODUCT == "google_sdk");
+    }
+}
