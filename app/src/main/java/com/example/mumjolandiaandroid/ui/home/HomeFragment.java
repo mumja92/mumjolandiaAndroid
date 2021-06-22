7753c491c04e720b1da1126fd36eae9269448585
mumja92 <mumja92@gmail.com>
2021-06-22 18:05:29 +0200

    Add mumjolandia console on Home page
    Python's mumjolandia is now capable of running local server for
    handling all possible commands via socket communication. Server
    only accepts local connections so ip is hardcoded to localhost.
    It is now possible to execute every command existing in python's
    mumjolandia remotely.

--- app/src/main/java/com/example/mumjolandiaandroid/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/mumjolandiaandroid/ui/home/HomeFragment.java
@@ -1,35 +1,74 @@
 package com.example.mumjolandiaandroid.ui.home;
 
+import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
 
 import com.example.mumjolandiaandroid.R;
+import com.example.mumjolandiaandroid.utils.Helpers;
+import com.example.mumjolandiaandroid.utils.MumjolandiaCommunicator;
 
 public class HomeFragment extends Fragment {
 
-    private HomeViewModel homeViewModel;
+    TextView viewResponse;
+    EditText userCommand;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        homeViewModel =
-                ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
-        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+        final Button buttonHomeSendCommand = root.findViewById(R.id.buttonHomeSendCommand);
+        buttonHomeSendCommand.setOnClickListener(new View.OnClickListener()
+        {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void onClick(View v)
+            {
+                String command = userCommand.getText().toString();
+                String local_ip = "127.0.0.1";
+                if (Helpers.isEmulator()){
+                    local_ip = "10.0.2.2";
+                }
+                new HomeBackgroundTask(local_ip, 3335, HomeFragment.this.getActivity()).execute(command);
+            }
+        });
+
+        viewResponse = root.findViewById(R.id.textViewHomeResponse);
+        userCommand = root.findViewById(R.id.editTextHomeCommand);
+        userCommand.setOnEditorActionListener(new EditText.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_DONE) {
+                    buttonHomeSendCommand.performClick();
+                    return true;
+                }
+                return false;
             }
         });
         return root;
+    }
+
+    private class HomeBackgroundTask extends MumjolandiaCommunicator {
+        private Context context;
+
+        public HomeBackgroundTask(String ip, int port, Context context) {
+            super(ip, port);
+            this.context = context;
+        }
+
+        public void onPostExecute(String result) {
+            TextView textView = (TextView) ((Activity) context).findViewById(R.id.textViewHomeResponse);
+            textView.setText(result);
+        }
     }
 }
